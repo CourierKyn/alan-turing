d8e262624b29c11763c32df97b2c30e90f6fa032
Liu Zhishang <1207252455@qq.com>
2019-07-03 01:05:57 +0800

    缩短问答时间

--- app.py
+++ app.py
@@ -1,28 +1,49 @@
-from flask import Flask, jsonify
+from flask import Flask, Response
 from flask import request
-import numpy as np
 import pandas as pd
-from backend.tet_bert_keras_sim import sim_two_question
 import json
+from backend.extract_keras_bert_feature import KerasBertVector
+import numpy as np
 
 app = Flask(__name__)
 
-
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
-    ques_1 = request.args.get("name") or ""
-    # ques_1 = input()
-    most_sim = sim_two_question(ques_1)
-    # print(most_sim)
-    return json.dumps(
+    ques_1 = request.get_json(force=True)['query']
+    def cosine_distance(v1, v2): # 余弦距离
+        if v1 and v2:
+            return np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))
+        else:
+            return 0
+    vector_1 = bert_vector.bert_encode([ques_1])
+    # q_and_a = pd.read_csv('Q_and_A.csv')
+    # queses_2 = q_and_a.Q
+    answers = q_and_a.A.tolist()
+    comp = []
+    for vector_2 in queses_2_vec:
+        sim = cosine_distance(vector_1[0], vector_2)
+        comp.append(sim)
+    comp = np.array(comp)
+    most_sim = answers[np.argmax(comp)]
+    return_value = json.dumps(
         {
             'answer': most_sim,
         }, ensure_ascii=False
     )
-
+    # print(most_sim)
+    # return Response(response=return_value, status=200, mimetype='text/html')
+    return return_value
 
 if __name__ == '__main__':
+    bert_vector = KerasBertVector()
+    q_and_a = pd.read_csv('Q&A.csv')
+    queses_2 = q_and_a.Q
+    answers = q_and_a.A.tolist()
+    queses_2_vec = []
+    for ques_2 in queses_2:
+        vector_2 = bert_vector.bert_encode([ques_2])
+        queses_2_vec.append(vector_2[0])
     app.run()
     # print('请输入问题：')
-    # a = hello_world()
+    # hello_world()
     # print(a)
